bd995435813073ce1d95bbcd42f567c304926439
lorettet <theo.lorette-froidevaux@insa-lyon.fr>
2018-06-05 22:18:55 +0200

    Création de classes + queue Cration des classes Town, PlayerTown, FarmTown et Island afin d'avoir une meilleur compréhension du code. Les villes sont stocké dans une queue pour mieux répartir les villages paysans.

--- auto5m.py
+++ auto5m.py
@@ -8,10 +8,63 @@ from time import sleep
 import threading 
 import datetime
 
-towns=[]
-farms=[]
-farmRelations={}
-townsMap={}
+from queue import Queue
+
+islands = []
+
+"""Class Town"""
+class Town:
+	def __init__(self, name, id, position):
+		self.name = name
+		self.id = id
+		self.position = position
+		
+	def __str__(self):
+		return self.name
+		
+		
+"""Class FarmTown"""	
+class FarmTown(Town):
+	def __init__(self, name, id, position, relation):
+		Town.__init__(self, name, id, position)
+		self.relation = relation
+
+"""Class PlayerTown"""
+class PlayerTown(Town):
+	def __init__(self, name, id, position):
+		Town.__init__(self, name, id, position)
+		
+"""Class Island"""
+class Island:
+	def __init__(self, position):
+		self.position = position
+		self.farms = []
+		self.q = Queue()
+		self.__dict__["towns"]=[]
+		
+	def addPlayerTown(self, town):
+		self.towns += town
+		
+	def addFarmTown(self, farm):
+		self.farms += town
+		
+	def __str__(self):
+		s =  "("+str(self.position[0])+","+str(self.position[1])+") : \n\t"
+		s += " ".join(str(e) for e in self.towns)
+		s += "\n\t"
+		s += " ".join(str(e) for e in self.farms)
+		return s
+
+	def getNext(self):
+		t = self.q.get_nowait()
+		self.q.put_nowait(t)
+		return t
+		
+	def __setattr__(self, name, value):
+		if name == "towns":
+			self.q.put_nowait(value[len(value)-1])
+		super().__setattr__(name, value)
+
 
 def usage():
 	print("Usage : "+sys.argv[0] + " login password")
@@ -22,24 +75,11 @@ def main(argv):
 		usage()
 		exit()
 	
+
 	connect.connect(argv[1],argv[2])
 	
-	global towns, farms, farmRelations, townsMap
-	data='{"collections":{"Towns":[]},"town_id":'+connect.tid+',"nl_init":false}'
-	r = connect.req.get("https://fr107.grepolis.com/game/frontend_bridge?town_id="+connect.tid+"&action=refetch&h="+connect.h+"&json="+data,headers={"X-Requested-With":"XMLHttpRequest"})
-	j = r.json()
-	for town in j["json"]["collections"]["Towns"]["data"]:
-		towns += [{"id":town["d"]["id"], "name":town["d"]["name"], "island":(town["d"]["island_x"],town["d"]["island_y"])}]
-		townsMap[town["d"]["id"]] = town["d"]["name"]
-
-	data = '{"types":[{"type":"easterIngredients"},{"type":"map","param":{"x":15,"y":7}},{"type":"bar"},{"type":"backbone"}],"town_id":'+connect.tid+',"nl_init":false}'
-	r = connect.req.post("https://fr107.grepolis.com/game/data?town_id="+connect.tid+"&action=get&h="+connect.h, data={"json":data}, headers={"X-Requested-With":"XMLHttpRequest"})
-	j = r.json()
-	for farm in j["json"]["backbone"]["collections"][25]["data"]:
-		farms += [{"id":farm["d"]["id"],"name":farm["d"]["name"], "island":(farm["d"]["island_x"],farm["d"]["island_y"])}]
-
-	for relation in j["json"]["backbone"]["collections"][26]["data"]: 
-		farmRelations[relation["d"]["farm_town_id"]] = relation["d"]["id"] 	
+	getInfo()
+	
 
 	print("Appuyez sur ENTRER pour arrêter le script")
 	threadRecolt = AutoRecolt()
@@ -49,7 +89,57 @@ def main(argv):
 	threadRecolt.event.set()
 	
 	
+def getInfo():
+	global islands
+	
+	"""Getting all player towns"""
+	data='{"collections":{"Towns":[]},"town_id":'+connect.tid+',"nl_init":false}'
+	r = connect.req.get("https://fr107.grepolis.com/game/frontend_bridge?town_id="+connect.tid+"&action=refetch&h="+connect.h+"&json="+data,headers={"X-Requested-With":"XMLHttpRequest"})
+	j = r.json()
+	towns = []
+	for town in j["json"]["collections"]["Towns"]["data"]:
+		towns += [PlayerTown(town["d"]["name"],town["d"]["id"], (town["d"]["island_x"],town["d"]["island_y"]))]
+
+	"""Getting all farm towns"""
+	data = '{"types":[{"type":"easterIngredients"},{"type":"map","param":{"x":15,"y":7}},{"type":"bar"},{"type":"backbone"}],"town_id":'+connect.tid+',"nl_init":false}'
+	r = connect.req.post("https://fr107.grepolis.com/game/data?town_id="+connect.tid+"&action=get&h="+connect.h, data={"json":data}, headers={"X-Requested-With":"XMLHttpRequest"})
+	j = r.json()
+	farms = []
+	for farm in j["json"]["backbone"]["collections"][25]["data"]:
+		farm_id = farm["d"]["id"]
+		relations = j["json"]["backbone"]["collections"][26]["data"]
+		index = 0
+		while(relations[index]["d"]["farm_town_id"] != farm_id):
+			index += 1
+			
+		farms += [FarmTown(farm["d"]["name"], farm_id, (farm["d"]["island_x"],farm["d"]["island_y"]), relations[index]["d"]["id"])]
 		
+		
+	"""Creating islands"""
+	for town in towns:
+		trouvee = False
+		for island in islands:
+			if(island.position == town.position):
+				island.towns += [town]
+				trouvee = True
+				break
+		if not trouvee:
+			island = Island(town.position)
+			islands += [island]
+			island.towns += [town]
+			
+	for farm in farms:
+		for island in islands:
+			if(island.position == farm.position):
+				island.farms += [farm]
+				trouvee = True
+				break
+				
+	for island in islands:
+		print(island)
+		print(island.q.qsize())
+	
+	
 class AutoRecolt(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
@@ -57,40 +147,34 @@ class AutoRecolt(threading.Thread):
 		
 	def run(self):
 		index_town = 0
-		global farms, farmRelations, towns, townsMap
+		global islands
 		
 		while not self.event.is_set():
 			print("Début de la récolte...")
-			for farm in farms:
-				relation = farmRelations[farm["id"]]
-				while(towns[index_town]["island"] != farm["island"]):
-					index_town+=1
-					if(index_town >= len(towns)):
-						index_town=0
-				self.getRessources(towns[index_town]["id"],farm["id"],relation)
-				index_town+=1
-				if(index_town >= len(towns)):
-					index_town=0
+			for island in islands:
+				for farm in island.farms:
+					self.getRessources(island.getNext(), farm)
 			currTime = datetime.datetime.now()
 			nextTime = currTime + datetime.timedelta(0,5*60)
 			print("Prochaine récolte à "+nextTime.strftime("%H:%M"))
 			self.event.wait(5*60)
 			
 				
-	def getRessources(self, tid, fid, r):
+	def getRessources(self, town, farm):
 		url = "https://fr107.grepolis.com/game/frontend_bridge?action=execute&h="+connect.h
-		global towns, farms, farmRelations, townsMap
+		global islands
 		
-		data = '{"model_url":"FarmTownPlayerRelation/'+str(r)+'","action_name":"claim","arguments":{"farm_town_id":"'+str(fid)+'","type":"resources","option":1},"town_id":"'+str(tid)+'","nl_init":true}'
+		data = '{"model_url":"FarmTownPlayerRelation/'+str(farm.relation)+'","action_name":"claim","arguments":{"farm_town_id":"'+str(farm.id)+'","type":"resources","option":1},"town_id":"'+str(town.id)+'","nl_init":true}'
 		r = connect.req.post(url, data={"json":data}, headers={"X-Requested-With":"XMLHttpRequest"})
 		resp = r.json()
 		if("success" in resp["json"]):
-			print("Ressources livrees vers "+ townsMap[tid])
+			print("Ressources livrees vers "+ town.name)
 		else:
 			print("Erreur : "+resp["json"]["error"])
 			
 	
-		
+
 if __name__ == "__main__":
 	main(sys.argv)
 			
+
